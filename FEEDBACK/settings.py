ef970f13d9e3a76fd0d8b270c41a404fd2466180
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2022-12-22 01:38:47 +0200

    inputs

--- FEEDBACK/settings.py
+++ FEEDBACK/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "reviews",
+    "profiles",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -117,3 +118,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+MEDIA_URL = "/user-media/"
---
+++ profiles/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
+from .forms import ProfileForm
+from .models import UserProfile
+# Create your views here.
+
+class CreateProfileView(CreateView):
+    model = UserProfile
+    fields = "__all__"
+    template_name = "profiles/create_profile.html"
+    success_url = "/profiles"
+
+# def store_file(file):
+#     with open(f"temp/{file.name}", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
+
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+
+#         return render(request, "profiles/create_profile.html", {"form":form})
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_file"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+        
+#         return render(request, "profiles/create_profile.html", {"form":submitted_form})
+
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profile.html"
+    context_object_name = "profiles"
